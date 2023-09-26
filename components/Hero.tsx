fc407d8d4f69d1d2ca42a09b618c41e33d472cee
Zk Rana <zkranao@gmail.com>
2023-09-26 16:49:44 +0600

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+          <h1 className='hero__title'> 
+            Hello
+          </h1>
+        </div>
+    </div>
+  )
+}
